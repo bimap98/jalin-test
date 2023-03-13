89e8c4298cf679254218657f6206361b51ca5456
PT. TODIS KARYA BANGSA <biput98@gmail.com>
2023-03-13 16:48:55 +0700

    change load json method

--- jalin-test-two/src/Jalin.java
+++ jalin-test-two/src/Jalin.java
@@ -35,51 +35,6 @@ public class Jalin {
 
   public Jalin() {
 
-  loadButton.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      JSONObject objectPayment = new JSONObject();
-      objectPayment.put("msisdn", "082285643282-payment");
-      objectPayment.put("amount", "00000000");
-      objectPayment.put("trxDate", "2020-06-09T11:16:33+07:00");
-      objectPayment.put("merchantTrxID", "0005502227972");
-
-
-      JSONObject objectCheck = new JSONObject();
-      objectCheck.put("msisdn", "082285643282-check");
-      objectCheck.put("amount", "00000000");
-      objectCheck.put("trxDate", "2020-06-09T11:16:33+07:00");
-      objectCheck.put("merchantTrxID", "0005502227972");
-
-      JSONObject objectRefund = new JSONObject();
-      objectRefund.put("msisdn", "082285643282-refund");
-      objectRefund.put("amount", "00000000");
-      objectRefund.put("trxDate", "2020-06-09T11:16:33+07:00");
-      objectRefund.put("merchantTrxID", "0005502227972");
-
-      JSONObject objectInquiry = new JSONObject();
-      objectInquiry.put("msisdn", "082285643282-inquiry");
-      objectInquiry.put("amount", "00000000");
-      objectInquiry.put("trxDate", "2020-06-09T11:16:33+07:00");
-      objectInquiry.put("merchantTrxID", "0005502227972");
-
-      JSONObject objectPaymentDebit = new JSONObject();
-      objectPaymentDebit.put("msisdn", "082285643282-payment debit");
-      objectPaymentDebit.put("amount", "00000000");
-      objectPaymentDebit.put("trxDate", "2020-06-09T11:16:33+07:00");
-      objectPaymentDebit.put("merchantTrxID", "0005502227972");
-
-      JSONObject[] choices = {objectPayment, objectCheck, objectRefund, objectInquiry, objectPaymentDebit};
-
-      JSONObject selectChoice = choices[messageEnum.getSelectedIndex()];
-
-      tfMessage.setText(CodeFormatter.formatToJSON(selectChoice.toString(4)));
-
-
-    }
-  });
-
-
     kirimButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
@@ -134,6 +89,49 @@ public class Jalin {
         tfInKey.setText("");
         tfHeader.setText("");
         tfResultHmac.setText("");
+      }
+    });
+    messageEnum.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+
+        JSONObject objectPayment = new JSONObject();
+        objectPayment.put("msisdn", "082285643282-payment");
+        objectPayment.put("amount", "00000000");
+        objectPayment.put("trxDate", "2020-06-09T11:16:33+07:00");
+        objectPayment.put("merchantTrxID", "0005502227972");
+
+
+        JSONObject objectCheck = new JSONObject();
+        objectCheck.put("msisdn", "082285643282-check");
+        objectCheck.put("amount", "00000000");
+        objectCheck.put("trxDate", "2020-06-09T11:16:33+07:00");
+        objectCheck.put("merchantTrxID", "0005502227972");
+
+        JSONObject objectRefund = new JSONObject();
+        objectRefund.put("msisdn", "082285643282-refund");
+        objectRefund.put("amount", "00000000");
+        objectRefund.put("trxDate", "2020-06-09T11:16:33+07:00");
+        objectRefund.put("merchantTrxID", "0005502227972");
+
+        JSONObject objectInquiry = new JSONObject();
+        objectInquiry.put("msisdn", "082285643282-inquiry");
+        objectInquiry.put("amount", "00000000");
+        objectInquiry.put("trxDate", "2020-06-09T11:16:33+07:00");
+        objectInquiry.put("merchantTrxID", "0005502227972");
+
+        JSONObject objectPaymentDebit = new JSONObject();
+        objectPaymentDebit.put("msisdn", "082285643282-payment debit");
+        objectPaymentDebit.put("amount", "00000000");
+        objectPaymentDebit.put("trxDate", "2020-06-09T11:16:33+07:00");
+        objectPaymentDebit.put("merchantTrxID", "0005502227972");
+
+        JSONObject[] choices = {objectPayment, objectCheck, objectRefund, objectInquiry, objectPaymentDebit};
+
+        JSONObject selectChoice = choices[messageEnum.getSelectedIndex()];
+
+        tfMessage.setText(CodeFormatter.formatToJSON(selectChoice.toString(4)));
+
       }
     });
   }
